188abf1c757cb43fd94370551a1988ca8b972f21
Cas de Pender <cas_de_pender@hotmail.com>
2017-05-04 11:44:44 +0200

    programmeren 4 les 2

---
+++ server.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Cas on 5/4/2017.
+ */
+/**
+ * Created by Cas on 5/2/2017.
+ */
+var express = require('express');
+var app = express();
+
+app.get('/', function(request, response) {
+    response.send('Hello Avans!');
+})
+
+app.get('/about', function(request, response) {
+    response.send('Written by <Cas>');
+})
+
+app.post('/', function(request, response) {
+    response.send('Hello Avans, POST request received!');
+})
+
+app.put('/', function(request, response) {
+    response.send('Hello Avans, PUT request received!');
+})
+
+app.listen(8080, function() {
+    console.log('Server app is listening on port 8080');
+})
